22971b46e66b108091f2419919c158d5e4434fae
Ous Bayaa <bayaaous@gmail.com>
2020-02-18 16:52:16 -0800

    added elements on PostsPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import DummyData from "../../dummy-data";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+  const [data] = useState(DummyData)
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map((element, index) => {
+        return <Post key= {index} post= {element}/>
+      })}
     </div>
   );
 };
